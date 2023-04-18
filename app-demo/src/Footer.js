06b5fbb30b7bb8e883114b133ce9888281e59493
KD <123972794+KaviousD@users.noreply.github.com>
2023-04-18 14:38:24 -0500

    Lab 01 updates

---
+++ app-demo/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                Author: Kavious moment
+            </footer>
+        )
+    }
+}
+
+export default Footer;
---
+++ app-demo/src/Main.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import HornedBeast from './HornedBeast';
+class Main extends React.Component {
+    render() {
+        return (
+            <main className="Animal image">
+                <HornedBeast keyword="triceratops" description="Horny dinosaur" title="A picture of a dinosuar" imageUrl="https://th.bing.com/th/id/OIP.0C2mH8bmiv2wT5tYS9mxTAHaEK?w=284&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"/>
+                
+
+            </main>
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Main;
