4c36a0e5ef8c18d1102b846b32565d2ac7fa1096
KD <123972794+KaviousD@users.noreply.github.com>
2023-04-24 13:36:16 -0500

    changes

--- app-demo/src/HornedBeast.js
+++ app-demo/src/HornedBeast.js
@@ -1,17 +1,21 @@
 import React from "react";
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 class HornedBeast extends React.Component {
 
   render() {
     return (
       <div className="Animalhorns">
-        <h2>{this.props.title}</h2>
+        <Card style={{width: '18rem'}}>
+         <h2>{this.props.title}</h2>
         <img
           src={this.props.imageUrl}
           alt={this.props.keyword}
           title={this.props.title}
         />
-        <p>{this.props.description}</p>
+        <p>{this.props.description}</p> 
+        </Card>
+        
       </div>
     );
   }
